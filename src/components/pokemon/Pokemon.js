afaa5ca2db04550a4322e511d947b98477c11201
novaeeken <nova_eeken@hotmail.com>
2022-12-13 12:05:23 +0100

    Update logo

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -3,44 +3,47 @@ import axios from 'axios';
 import './Pokemon.css';
 
 function Pokemon({ endpoint }) {
-  const [pokemon, setPokemon] = useState(null);
+  const [pokemon, setPokemon] = useState({});
 
   useEffect(() => {
     console.log(endpoint);
+
     async function fetchData() {
       try {
         const { data } = await axios.get(endpoint);
         setPokemon(data);
-      } catch(e) {
+      } catch (e) {
         console.error(e);
       }
     }
 
-    fetchData();
+    if (endpoint) {
+      fetchData();
+    }
   }, [endpoint]);
 
 
   return (
     <section className="poke-card">
-      {pokemon &&
+      {Object.keys(pokemon).length > 0 &&
         <>
-        <h2>{pokemon.name}</h2>
-        <img
-          alt="Afbeelding pokémon"
-          src={pokemon.sprites.front_default}
-        />
-        <p><strong>Moves: </strong>{pokemon.moves.length}</p>
-        <p><strong>Weight: </strong>{pokemon.weight}</p>
-        <p><strong>Abilities: </strong></p>
-        <ul>
-          {pokemon.abilities.map((ability) => {
-            return (
-              <li key={`${ability.ability.name}-${pokemon.name}`}>
-                {ability.ability.name}
-              </li>
-            )
-          })}
-        </ul>
+          <h2>{pokemon.name}</h2>
+          <img
+            alt="Afbeelding pokémon"
+            src={pokemon.sprites.front_default}
+          />
+          <p><strong>Moves: </strong>{pokemon.moves.length}</p>
+          <p><strong>Weight: </strong>{pokemon.weight}</p>
+          <p><strong>Abilities: </strong></p>
+          <ul>
+            {pokemon.abilities.map((ability) => {
+              return (
+                <li key={`${ability.ability.name}-${pokemon.name}`}>
+                  {ability.ability.name}
+                </li>
+              )
+            })}
+          </ul>
         </>
       }
     </section>
